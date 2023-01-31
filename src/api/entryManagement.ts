17354514783872bc89e545135bd82f3e66f07bdc
七七 <1772014415@qq.com>
2023-01-31 23:04:16 +0800

    词条管理100% 接口功能测试完成

---
+++ src/api/entryManagement.ts
@@ -0,0 +1,43 @@
+import request from "../utils/request"
+//词条分页查询
+export function getEntryListPageAPI(
+    params: {
+      //分页参数
+      pageNum:number,
+      pageSize:number,
+      //词条名称
+      entryName:string
+    }) {
+    return request({
+      url: '/service_book/service/entry/getEntryListPage',
+      method: 'post',
+      params
+    })
+  }
+
+  //删除词条
+  export function deleteEntryAPI(
+    params:{
+      id:string,
+    }
+  ) {
+    return request({
+      url: '/service_book/service/entry'+params.id,
+      method: 'delete',
+    })
+  }
+
+  //词条下的所属关系树
+  export function getEntryTreeListAPI(
+    param: {
+      //词条id
+      id:string,
+      //词条父id
+      parentId:string
+    }) {
+    return request({
+      url: '/service_book/service/entry/entryTreeList',
+      method: 'get',
+      params:param
+    })
+  }
